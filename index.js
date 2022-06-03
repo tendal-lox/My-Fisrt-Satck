b816afcf8dadd6614a365230115130a39d0381ad
Ali Fathi <ali-fathi77@outlook.com>
2022-06-03 13:59:40 +0430

    new api added

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-function stack(){
-    this.item = []
+function stack(limit = 10){
+    this.limit = limit
+    this.item = Array()
 }
 
 stack.prototype.push = function(input){
+    if(this.isFull()){
+        return -1
+    }
     this.item[this.item.length] = input
     console.log(this.item)
+    return 1
 }
 
 stack.prototype.pop = function(){
     let output = this.item[this.item.length-1]
     this.item = this.item.splice(0, this.item.length-1)
     return output
+}
+
+stack.prototype.size = function(){
+    return this.item.length
+}
+
+stack.prototype.isEmpty = function(){
+    return this.size() === 0
+}
+
+stack.prototype.isFull = function(){
+    return this.size() === this.limit
 }
 
 module.exports = stack
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const stack = require('./index')
 const chalk = require('chalk')
 
-const s = new stack()
-
+const s = new stack(2)
+console.log('first is empty', s.isEmpty(), s.size())
 s.push({
     name : 'ali'
 })
@@ -15,14 +15,20 @@ s.push({
     name : 'ahamd'
 })
 
+// console.log('isempty1', s.isEmpty(), s.size())
+
 let data = s.pop()
+
+// console.log('isempty2', s.isEmpty(), s.size())
 
 let data2 = s.pop()
 
 let data3 = s.pop()
 
-console.log(chalk.red('hello'),data)
+console.log('isempty3', s.size())
 
-console.log(chalk.black('hello this is second pop'), data2)
+console.log(chalk.red('hello'), s.size())
 
-console.log(data3)
+console.log(chalk.white('hello this is second pop'), data2, s.size())
+
+console.log(data3, s.size())
